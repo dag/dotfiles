3de2485a5ad1565ac32177be7268b981318c3d91
Dag Odenhall <dag.odenhall@gmail.com>
2012-04-01 02:24:36 +0200

    make tree more useful for haskell

--- .bash/aliases.sh
+++ .bash/aliases.sh
@@ -4,7 +4,7 @@ alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-alias tree='tree -ACI "*.pyc"'
+alias tree='tree -AC -I "*.pyc|_darcs|cabal-dev|dist|state"'
 
 alias apt-get='sudo apt-get'
 
